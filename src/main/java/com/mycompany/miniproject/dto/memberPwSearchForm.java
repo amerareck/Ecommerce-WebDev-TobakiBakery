fca041430d0f65f579e042b83f2ae30b611983f5
AnJungGeon <gunn0128@gmail.com>
2024-09-05 23:38:37 +0900

    Feat: 회원비밀번호 찾기 폼 객체 추가
    회원의 비밀번호를 찾는 폼 객체를 추가했습니다.

---
+++ src/main/java/com/mycompany/miniproject/dto/memberPwSearchForm.java
@@ -0,0 +1,11 @@
+package com.mycompany.miniproject.dto;
+
+import lombok.Data;
+
+@Data
+public class memberPwSearchForm {
+	private String username;
+	private String name;
+	private String quesetion;
+	private String answer;
+}
