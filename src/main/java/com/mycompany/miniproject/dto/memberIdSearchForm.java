a59e91b49072642f63dfa431f2bdf817d0474af8
AnJungGeon <gunn0128@gmail.com>
2024-09-05 23:39:39 +0900

    Feat: 아이디 찾기 폼 객체 추가
    회원의 아이디 찾기 폼 객체를 추가했습니다.

---
+++ src/main/java/com/mycompany/miniproject/dto/memberIdSearchForm.java
@@ -0,0 +1,10 @@
+package com.mycompany.miniproject.dto;
+
+import lombok.Data;
+
+@Data
+public class memberIdSearchForm {
+	private String name;
+	private String quesetion;
+	private String answer;
+}
