d213833c4665806fe69b918a8901d59a718ebda7
amerareck <tjdgus510@gmail.com>
2024-09-05 22:51:41 +0900

    feat: 사용자 폼 데이터를 담는 객체 생성
    사용자의 입력 데이터를 담아줄 form 객체를 생성했습니다.

---
+++ src/main/java/com/mycompany/miniproject/dto/HelpdeskForm.java
@@ -0,0 +1,12 @@
+package com.mycompany.miniproject.dto;
+
+public class HelpdeskForm {
+	String helpdeskTitle;
+	String helpdeskContent;
+	boolean lock_state;
+	String memberId;
+	
+	String imageOriginalName;
+	String imageType;
+	byte[] imageData;
+}
---
+++ src/main/java/com/mycompany/miniproject/dto/NoticeForm.java
@@ -0,0 +1,11 @@
+package com.mycompany.miniproject.dto;
+
+public class NoticeForm {
+	String noticeTitle;
+	String noticeContent;
+	String memberId;
+	
+	String imageOriginalName;
+	String imageType;
+	byte[] imageData;
+}
